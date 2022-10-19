1434152d0d7182210b954d71de4f9b4ce3ca8ce8
Oleksandr Kurtsev <kurtsev0103@gmail.com>
2022-10-19 18:36:42 +0200

    Created project

---
+++ StackViewController/StackViewController.h
@@ -0,0 +1,18 @@
+//
+//  StackViewController.h
+//  StackViewController
+//
+//  Created by Oleksandr Kurtsev on 19/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StackViewController.
+FOUNDATION_EXPORT double StackViewControllerVersionNumber;
+
+//! Project version string for StackViewController.
+FOUNDATION_EXPORT const unsigned char StackViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StackViewController/PublicHeader.h>
+
+
