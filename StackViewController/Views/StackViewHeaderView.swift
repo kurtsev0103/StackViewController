25c40b95778fa42ed8f20dc7cb4796ff1091e629
Oleksandr Kurtsev <kurtsev0103@gmail.com>
2023-06-02 10:30:26 +0200

    Changed grabber UI

--- StackViewController/Views/StackViewHeaderView.swift
+++ StackViewController/Views/StackViewHeaderView.swift
@@ -17,7 +17,7 @@ final class StackViewHeaderView: UIView {
     
     private enum Configuration {
         static let grabberHeight: CGFloat = 4.0
-        static let grabberWidth: CGFloat = 40.0
+        static let grabberWidth: CGFloat = 36.0
     }
     
     // MARK: - Initialization
@@ -52,7 +52,35 @@ final class StackViewHeaderView: UIView {
 enum Subviews {
     static var grabber: UIView {
         let view = UIView()
-        view.backgroundColor = .lightGray
+        view.backgroundColor = .init(hex: "#D9D9D9")
         return view
+    }
+}
+
+fileprivate extension UIColor {
+    
+    convenience init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+        
+        self.init(
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            alpha: Double(a) / 255
+        )
     }
 }
